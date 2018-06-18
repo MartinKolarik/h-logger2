0f6b460ac1fe53f36ef15121c7e3a2f50d6028e9
Martin Kolárik <martin@kolarik.sk>
2018-06-18 19:23:38 +0200

    Update Writer interface

--- src/Writer.js
+++ src/Writer.js
@@ -3,6 +3,11 @@ class Writer {
 		this.level = level;
 		this.options = options;
 	}
+
+	// eslint-disable-next-line no-unused-vars
+	write (logger, level, message, error, context) {
+		// TODO: Implement this method.
+	}
 }
 
 module.exports = Writer;
