ea5b245e4489459c96903323887e586d5c5c5d5a
Martin Kolárik <martin@kolarik.sk>
2024-12-11 20:40:38 +0100

    Add missing export

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const ConsoleWriter = require('./ConsoleWriter');
 const Writer = require('./Writer');
 
 module.exports = Logger;
+module.exports.Logger = Logger;
 module.exports.ConsoleWriter = ConsoleWriter;
 module.exports.Writer = Writer;
